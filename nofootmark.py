cd29b80916380fe1c5e1ff84c93e6cfe91c32cbe
Coleman Donaldson <coleman.donaldson@gmail.com>
2020-03-03 12:56:45 +0100

    Update nofootmark.py

--- nofootmark.py
+++ nofootmark.py
@@ -2,15 +2,15 @@
 
 # Create a function to remove footnote markers
 
-def stripFootNote(text):
+def stripFootMark(text):
 
-    outsideFootMark = 1
+	outsideFootMark = 1
 	noFootMark = ""
 
 	for char in text:
-		if char == '^':
+		if char == '[':
 			outsideFootMark = 0
-		elif (outsideFoot == 0 and char == ']'):
+		elif (outsideFootMark == 0 and char == ']'):
 			outsideFootMark == 1
 		elif outsideFootMark == 0:
 			continue
