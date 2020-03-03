8875980c175753c6211a398575d8d77b01e93d04
Coleman Donaldson <coleman.donaldson@gmail.com>
2020-03-03 12:35:02 +0100

    Update nofootnotes.py
    Added comments

--- nofootnotes.py
+++ nofootnotes.py
@@ -2,19 +2,20 @@
 
 # A script to remove pandoc footnotes from a plaintext file
 
+# Create a function to remove lines of footnote text
 def stripFoots(text):
 
     outsideFoot = 1
     noFoot = ""
 
     for char in text:
-        if char == '[':
+        if char == '[': # If you hit [ then tell the computer you are inside a foonote
             outsideFoot = 0
-        elif (outsideFoot == 0 and char == '\n'):
+        elif (outsideFoot == 0 and char == '\n'): # Otherwise if you are inside a footnote and you hit a new line then tell the computer that it is outside a footnote again
             outsideFoot = 1
-        elif outsideFoot == 0:
+        elif outsideFoot == 0: # Otherwise if you are inside a foot then just keep going
             continue
-        else:
+        else: # Otherwise (that is, if you are not inside a footnote then write the character to our new file)
             noFoot += char
             continue
     return(noFoot)
