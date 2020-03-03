0e58c114e47cd7148d8c9112f2c0c5a9ef697898
Coleman Donaldson <coleman.donaldson@gmail.com>
2020-03-03 13:00:32 +0100

    Update cleanup.py
    Will remove header markers and horizontal line breaks

--- cleanup.py
+++ cleanup.py
@@ -2,7 +2,7 @@
 
 # A Python program to manipulate the Jula Sura 2 markdown to remove foonotes and put it into a tabular data form
 
-import readmd, nofootnotes, removelines
+import readmd, nofootnotes, nofootmark, removelines
 
 # Create a blank variable "file"
 print(">>>>>> Create a blank file")
@@ -17,18 +17,23 @@ file = readmd.openFile(file)
 
 print(">>>>>> Here are the first 100 characters of \"file:" + "\n" + file[0:100])
 
-# Run stripFoots to remove foonotes
+# Run stripFoots to remove footnotes
 print(">>>>>> Run the stripFoots function on \"file\"")
 file = nofootnotes.stripFoots(file)
 
 print(">>>>>> Here are the contents without footnotes:\n" + file[1:100])
 
 # Run noWhiteLines to remove blank lines
-
 print(">>>>>>> Removing whitelines in the file...")
 file = removelines.noWhiteLines(file)
 
-print(">>>>>>> Saving the cleaned up version without footnotes and blank lines...")
+# Remove header symbols
+file = file.replace('### ','')
+
+# Remove dividers
+file= file.replace('---\n','')
+
+print(">>>>>>> Saving the cleaned up version")
 
 f = open("new-text.md","w")
 f.write(file)
