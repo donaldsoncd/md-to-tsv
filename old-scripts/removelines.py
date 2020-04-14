ab936459dd9059e6ba9c8033e077340d40cc7b6c
Coleman <coleman.donaldson@gmail.com>
2020-04-14 14:20:55 +0200

    Delete old scripts from 1st attempt

--- old-scripts/removelines.py
+++
@@ -1,8 +0,0 @@
-# removelines.py
-
-# a script that removes blank whitelines in a text file
-
-def noWhiteLines(text):
-	text = text.replace('\n\n\n','\n')
-	text = text.replace('\n\n','\n')
-	return(text)
